f5c18d56c74e430b0c09a8c1358cb98b22f6c24c
Sushil Kumar Shah <sushil.shah@incedoinc.com>
2018-02-05 16:42:36 +0530

    Added service API call

--- src/app/pages/home/dashboard/dashboard.component.ts
+++ src/app/pages/home/dashboard/dashboard.component.ts
@@ -16,16 +16,16 @@ export class DashboardComponent implements OnInit {
 
   deviceCount: number;
   alertCount: number;
-
-  devices: any = [{"updated":1516814540,"alerts":{},"z_mms":4,"y_mms":11,"x_mms":11,"id":"222","x_hz":25,
-  "name":"Sensor-300577","z_hz":36,"y_hz":7},{"updated":1516814095,"alerts":{"y_hz":40,"z_mms":18,"x_mms":24,
-  "x_hz":55},"z_mms":18,"y_mms":2,"x_mms":23,"id":"555","x_hz":55,"name":"Sensor-195732","z_hz":11,"y_hz":40},
-  {"updated":1516813894,"alerts":{"y_hz":40,"z_mms":17},"z_mms":17,"y_mms":4,"x_mms":2,"id":"444","x_hz":35,
-  "name":"Sensor-300579","z_hz":15,"y_hz":40},{"updated":1516814095,"alerts":{"z_mms":22,"x_mms":24,"z_hz":60},
-  "z_mms":22,"y_mms":10,"x_mms":24,"id":"111","x_hz":10,"name":"Sensor-300578","z_hz":60,"y_hz":24},
-  {"id":"OM","thiscolumn":{"message":"Missing required node: state","code":400}},
-  {"updated":1516814540,"alerts":{"x_hz":50},"z_mms":6,"y_mms":20,"x_mms":17,"id":"333","x_hz":50,
-  "name":"Sensor-300575","z_hz":13,"y_hz":8}]
+  devices: any;
+  // devices: any = [{"updated":1516814540,"alerts":{},"z_mms":4,"y_mms":11,"x_mms":11,"id":"222","x_hz":25,
+  // "name":"Sensor-300577","z_hz":36,"y_hz":7},{"updated":1516814095,"alerts":{"y_hz":40,"z_mms":18,"x_mms":24,
+  // "x_hz":55},"z_mms":18,"y_mms":2,"x_mms":23,"id":"555","x_hz":55,"name":"Sensor-195732","z_hz":11,"y_hz":40},
+  // {"updated":1516813894,"alerts":{"y_hz":40,"z_mms":17},"z_mms":17,"y_mms":4,"x_mms":2,"id":"444","x_hz":35,
+  // "name":"Sensor-300579","z_hz":15,"y_hz":40},{"updated":1516814095,"alerts":{"z_mms":22,"x_mms":24,"z_hz":60},
+  // "z_mms":22,"y_mms":10,"x_mms":24,"id":"111","x_hz":10,"name":"Sensor-300578","z_hz":60,"y_hz":24},
+  // {"id":"OM","thiscolumn":{"message":"Missing required node: state","code":400}},
+  // {"updated":1516814540,"alerts":{"x_hz":50},"z_mms":6,"y_mms":20,"x_mms":17,"id":"333","x_hz":50,
+  // "name":"Sensor-300575","z_hz":13,"y_hz":8}]
 
 
 
@@ -44,6 +44,7 @@ export class DashboardComponent implements OnInit {
     .subscribe(data => {
         let response = data.json();
         this.deviceCount = response.length;
+        this.devices = response;
     }, err => {
         console.log(err);
     });
